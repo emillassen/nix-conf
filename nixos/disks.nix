570c23a2fc22c5613e59bf37b54dd11e36f48462
Emil Lassen <155289+emillassen@users.noreply.github.com>
2024-06-05 10:15:08 +0200

    Disko change

--- nixos/disks.nix
+++ nixos/disks.nix
@@ -3,7 +3,7 @@
     disk = {
       vdb = {
         type = "disk";
-        device = "/dev/vdb";
+        device = "/dev/nvme0n1";
         content = {
           type = "gpt";
           partitions = {

825f693f150d048223a75f9d6c7f56e9a9e81924
Emil Lassen <155289+emillassen@users.noreply.github.com>
2024-06-06 20:47:13 +0200

    Misc updates

--- README.md
+++ README.md
@@ -11,10 +11,19 @@
 1. `cdnix && git remote set-url origin git@github.com:emillassen/nix-conf.git`
 
 ## Manual changes
-1. gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
-* Can't be set using home-manager, as it is reset to 1.00 during rebuilds.. also works better than fractional scaling
 1. Enroll fingerprints using `fprintd-enroll`
 1. Login to Nextcloud Desktop client
+
+### GNOME changes
+1. `gsettings set org.gnome.desktop.interface color-scheme prefer-dark`
+1. `gsettings set org.gnome.desktop.interface show-battery-percentage true`
+1. `gsettings set org.gnome.desktop.interface text-scaling-factor 1.25`
+1. `gsettings set org.gnome.desktop.peripherals.touchpad speed 0.1`
+1. `gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true`
+1. `gsettings set org.gnome.desktop.peripherals.mouse accel-profile flat`
+1. `gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"`
+1. `gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false`
+1. `gsettings set org.gnome.settings-daemon.plugins.power idle-dim false`
 
 ### YubiKey Setup
 1. Download pub.asc from Bitwarden
--- home-manager/config/gnome/gnomesettings.nix
+++ home-manager/config/gnome/gnomesettings.nix
@@ -4,41 +4,41 @@
   # https://hoverbear.org/blog/declarative-gnome-configuration-in-nixos/
   # Use `dconf watch /` to track stateful changes you are doing, then set them here.
   dconf.settings = {
-    # Disables automatic screen brightness in Gnome Settings -> Power 
+    # Enables dark mode
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+    # Shows the excact charge level in the top bar
+    "org/gnome/desktop/interface" = {
+      show-battery-percentage = "true";
+    };
+    # Changes the text scaling factor to 1.25
+    "org/gnome/desktop/interface" = {
+      text-scaling-factor = "1.25";
+    };
+    # Sets Touchpad speed
+    "org/gnome/desktop/peripherals/touchpad" = {
+      speed = "0.1";
+    };
+    # Enables tap to click using the trackpad
+    "org/gnome/desktop/peripherals/touchpad" = {
+      tap-to-click = "true";
+    };
+    # Disables mouse acceleration
+    "org/gnome/desktop/peripherals/mouse" = {
+      accel-profile = "flat";
+    };
+    # Enables fractional scaling in Gnome Settings -> Displays
+    "org/gnome/mutter" = {
+      experimental-features = [ "scale-monitor-framebuffer" ];
+    };
+    # Disables automatic screen brightness in Gnome Settings -> Power
     "org/gnome/settings-daemon/plugins/power" = {
       ambient-enabled = "false";
     };
     # Disables dimming of screen in Gnome Settings -> Power
     "org/gnome/settings-daemon/plugins/power" = {
       idle-dim = "false";
-    };
-    # Shows the excact charge level in the top bar
-    "org/gnome/desktop/interface" = {
-      show-battery-percentage = "true";
-    };
-    # Enables fractional scaling in Gnome Settings -> Displays
-    "org/gnome/mutter" = {
-      experimental-features = [ "scale-monitor-framebuffer" ];
-    };
-    # Changes the text scaling factor to 1.25
-    #"org/gnome/desktop/interface" = {
-    #  text-scaling-factor = "1.25";
-    #};
-    # Enables tap to click using the trackpad
-    "org/gnome/desktop/peripherals/touchpad" = {
-      tap-to-click = "true";
-    };
-    # Enables dark mode
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-    # Sets Touchpad speed
-    "org/gnome/desktop/peripherals/touchpad" = {
-      speed = "0.1";
-    };
-    # Disables mouse acceleration
-    "org/gnome/desktop/peripherals/mouse" = {
-      accel-profile = "flat";
     };
   };
 }

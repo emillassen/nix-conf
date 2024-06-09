7683d7160dbd1f5fca3e6318807d6c07a8a9da9a
Emil Lassen <155289+emillassen@users.noreply.github.com>
2024-06-09 19:05:20 +0200

    Update default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,7 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
-# You can build them using 'nix build .#example'
+# You can refer to packages from anywhere on your home-manager/nixos configurations,
+# build them with nix build .#package-name, or bring them into your shell with nix shell .#package-name
 pkgs: {
   # example = pkgs.callPackage ./example { };
+  vuescan = pkgs.callPackage ./vuescan { };
 }
